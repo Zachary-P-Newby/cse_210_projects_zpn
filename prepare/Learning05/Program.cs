0c1074fe4d454adc3c96ee3367cc9fde4f6b16a2
Zachary-P-Newby <zpnewby@outlook.com>
2023-06-14 23:28:55 -0500

    Completed polymorphisim activity for week 9

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> myList = new List<Shape>();
+
+        myList.Add(new Square("red", 3));
+        myList.Add(new Circle("Blue", 2));
+        myList.Add(new Rectangle("Yellow", 2.60, 5));
+
+        foreach(Shape item in myList){
+            Console.WriteLine($"Color: {item.GetColor()}\nArea: {item.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/rectangle.cs
@@ -0,0 +1,15 @@
+public class Rectangle: Shape{
+
+    double _length;
+    double _width;
+
+    public Rectangle(String color, double length, double width): base(color){
+        _length = length;
+        _width = width;
+    }
+
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
